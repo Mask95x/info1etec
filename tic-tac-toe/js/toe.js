f398b52016f31d3dec91b7a423c5e96d547927ce
Mask95x <106964531+Mask95x@users.noreply.github.com>
2022-08-08 08:38:48 -0300

    Update toe.js

--- tic-tac-toe/js/toe.js
+++ tic-tac-toe/js/toe.js
@@ -15,20 +15,35 @@ function old(element) {
         } else {
             current = "O";
         }
+
+        winner();
     }
 }
 function winner() {
-    /*var c_1 = document.getElementById("c_1").innerHTML;
-    var c_2 = document.getElementById("c_2").innerHTML;
-    var c_3 = document.getElementById("c_3").innerHTML;
-    var c_4 = document.getElementById("c_4").innerHTML;
-    var c_5 = document.getElementById("c_5").innerHTML;
-    var c_6 = document.getElementById("c_6").innerHTML;
-    var c_7 = document.getElementById("c_7").innerHTML;
-    var c_8 = document.getElementById("c_8").innerHTML;
-    var c_9 = document.getElementById("c_9").innerHTML;
+    var c_1 = document.getElementById("c_1").getAttribute("data-value");
+    var c_2 = document.getElementById("c_2").getAttribute("data-value");
+    var c_3 = document.getElementById("c_3").getAttribute("data-value");
+    var c_4 = document.getElementById("c_4").getAttribute("data-value");
+    var c_5 = document.getElementById("c_5").getAttribute("data-value");
+    var c_6 = document.getElementById("c_6").getAttribute("data-value");
+    var c_7 = document.getElementById("c_7").getAttribute("data-value");
+    var c_8 = document.getElementById("c_8").getAttribute("data-value");
+    var c_9 = document.getElementById("c_9").getAttribute("data-value");
 
-    if (c_1 != "" && c_2 != "" && c_3 != "" && c_1 == c_2 && c_1 == c_3 ||
+    if (c_1 != null && c_2 != null && c_3 != null && c_1 == c_2 && c_1 == c_3 ||
+        c_4 != null && c_5 != null && c_6 != null && c_4 == c_5 && c_4 == c_6 ||
+        c_7 != null && c_8 != null && c_9 != null && c_7 == c_8 && c_7 == c_9 ||
+        c_1 != null && c_4 != null && c_7 != null && c_1 == c_4 && c_1 == c_7 ||
+        c_2 != null && c_5 != null && c_8 != null && c_2 == c_5 && c_2 == c_8 ||
+        c_3 != null && c_6 != null && c_9 != null && c_3 == c_6 && c_3 == c_9 ||
+        c_1 != null && c_5 != null && c_9 != null && c_1 == c_5 && c_1 == c_9 ||
+        c_3 != null && c_5 != null && c_7 != null && c_3 == c_5 && c_3 == c_7
+        ) {
+        alert("FIM");
+        finish = 1;
+    }
+
+    /*if (c_1 != "" && c_2 != "" && c_3 != "" && c_1 == c_2 && c_1 == c_3 ||
         c_4 != "" && c_5 != "" && c_6 != "" && c_4 == c_5 && c_4 == c_6 ||
         c_7 != "" && c_8 != "" && c_9 != "" && c_7 == c_8 && c_7 == c_9 ||
         c_1 != "" && c_4 != "" && c_7 != "" && c_1 == c_4 && c_1 == c_7 ||
@@ -41,7 +56,7 @@ function winner() {
         finish = 1;
     }*/
 
-    //Verificação Horizontal
+    /*//Verificação Horizontal
     var tics = document.getElementsByClassName("tic");
     for (let i = 0; i < tics.length; i++) {
         let tacs = tics[i].children;
@@ -59,7 +74,7 @@ function winner() {
     }
 
     //Verificação Vertical
-    /*for (let i = 0; i < tics.length; i++) {
+    for (let i = 0; i < tics.length; i++) {
         let tacs = tics[0].children;
         let val = tacs[i].innerHTML;
         for (let x = 0; x < tacs.length; x++) {
